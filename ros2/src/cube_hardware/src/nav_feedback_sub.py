df9149789c3ddb8294f1013a7794e1da0d22a59d
Jatin Patil <jatinpatil2003@gmail.com>
2023-12-09 15:54:28 +0530

    changed

--- ros2/src/cube_hardware/src/nav_feedback_sub.py
+++ ros2/src/cube_hardware/src/nav_feedback_sub.py
@@ -15,13 +15,22 @@ class NavFeedbackNode(Node):
         self.subscription  # prevent unused variable warning
 
         self.feedback = Int64()
+
         self.pub = False
 
         # Publisher
         self.publisher = self.create_publisher(Int64, '/nav_feedback', 10)
+
+        # Timer
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
         self.get_logger().info('Started nav_feedback_sub')
 
             
+    def timer_callback(self):
+        self.publisher.publish(self.feedback)
+        self.get_logger().info(' published!!')
 
     def listener_callback(self, msg):
         self.get_logger().info('Received feedback: "%s"' % msg.data)
