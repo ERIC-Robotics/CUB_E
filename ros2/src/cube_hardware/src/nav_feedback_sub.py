7eb5396ec559d34f940a45ab6adfdd3907c7a858
Jatin Patil <jatinpatil2003@gmail.com>
2023-12-09 15:31:14 +0530

    updated nav_feedback_sub

--- ros2/src/cube_hardware/src/nav_feedback_sub.py
+++ ros2/src/cube_hardware/src/nav_feedback_sub.py
@@ -14,14 +14,24 @@ class NavFeedbackNode(Node):
             10)
         self.subscription  # prevent unused variable warning
 
+        self.feedback = Int64()
+        self.pub = False
+
         # Publisher
         self.publisher = self.create_publisher(Int64, '/nav_feedback', 10)
         self.get_logger().info('Started nav_feedback_sub')
 
+        while True:
+            if self.pub:
+                self.publisher.publish(self.feedback)
+                self.pub = False
+                self.get_logger().info('Data published !!')
+
     def listener_callback(self, msg):
         self.get_logger().info('Received feedback: "%s"' % msg.data)
-        self.publisher.publish(msg)
-        self.get_logger().info('Data published !!')
+        self.pub = True
+        self.feedback = msg
+        
 
 def main(args=None):
     rclpy.init(args=args)
