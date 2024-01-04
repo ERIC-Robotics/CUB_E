aa7b887a054798175b0fb591e9e1c6a36f18ff03
Jatin Patil <jatinpatil2003@gmail.com>
2024-01-04 14:21:56 +0530

    Added 5 sec green led and idle condition

--- ros2/src/cube_hardware/src/navigation_action.py
+++ ros2/src/cube_hardware/src/navigation_action.py
@@ -1,4 +1,5 @@
 import rclpy
+import time
 from rclpy.action import ActionClient
 from geometry_msgs.msg import PoseStamped
 from nav2_msgs.action import NavigateToPose
@@ -20,9 +21,22 @@ class NavigateToGoalClient:
         # self.feedback_publisher.publish(String(data="Succeeded"))
         self.published = False
         self.nav_status = Int64()
+        self.nav_status.data = 99
+        self.publish_green_5sec = False
+
+        self.previous_time = time.time()
+        self.now_time = time.time()
 
     def feedback_publisher_timer_pub(self):
-        self.feedback_publisher.publish(self.nav_status)
+        if self.publish_green_5sec:
+            green_msg = Int64()
+            green_msg.data = 1
+            self.feedback_publisher.publish(green_msg)
+            self.now_time = time.time()
+            if self.now_time - self.previous_time >= 5:
+                self.publish_green_5sec = False
+        else:        
+            self.feedback_publisher.publish(self.nav_status)
 
     def goal_pose_callback(self, msg):
         # self.node.get_logger().info('Received goal pose: "{0}"'.format(msg))
@@ -33,16 +47,6 @@ class NavigateToGoalClient:
         self.send_goal_future = self.action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback, )
         self.send_goal_future.add_done_callback(self.goal_response_callback)
 
-    # def send_goal(self):
-    #     goal_msg = NavigateToPose.Goal()
-    #     goal_msg.pose.header.frame_id = 'map'  # Set the frame_id according to your map frame
-    #     goal_msg.pose.pose.position.x = -2.0  # Set the x-coordinate of the goal pose
-    #     goal_msg.pose.pose.position.y = -3.0  # Set the y-coordinate of the goal pose
-    #     goal_msg.pose.pose.orientation.w = 1.0  # Set the orientation of the goal pose
-
-    #     self.send_goal_future = self.action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback, )
-    #     self.send_goal_future.add_done_callback(self.goal_response_callback)
-
     def feedback_callback(self, feedback_msg):
         # print(f'Received feedback: {feedback_msg.feedback}')
         # print()
@@ -50,26 +54,26 @@ class NavigateToGoalClient:
         # print(feedback_msg)
         if not self.published:
             self.nav_status.data = 2
-            self.feedback_publisher.publish(self.nav_status)
+            # self.feedback_publisher.publish(self.nav_status)
             self.published = True
         pass
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
-        print(goal_handle)
+        # print(goal_handle)
         if not goal_handle.accepted:
             print('Goal was rejected')
             self.nav_status.data = 0
-            self.feedback_publisher.publish(self.nav_status)
+            # self.feedback_publisher.publish(self.nav_status)
             return
 
-        print('Goal was accepted. Waiting for result...')
+        # print('Goal was accepted. Waiting for result...')
         result_future = goal_handle.get_result_async()
         result_future.add_done_callback(self.result_callback)
 
     def result_callback(self, future):
         result = future.result()
-        print(result)
+        # print(result)
         # 0 — Unknown.
         # 1 — Accepted.
         # 2 — Executing.
@@ -78,16 +82,19 @@ class NavigateToGoalClient:
         # 5 — Canceled.
         # 6 — Aborted.    
         if result.status == 4:
-            print('Goal was successful!')
+            # print('Goal was successful!')
             self.nav_status.data = 1
-            self.feedback_publisher.publish(self.nav_status)
+            self.publish_green_5sec = True
+            self.previous_time = time.time()
+            # self.feedback_publisher.publish(self.nav_status)
             
         else:
-            print(f'Goal failed with result code: {result}')
+            # print(f'Goal failed with result code: {result}')
             self.nav_status.data = 0
-            self.feedback_publisher.publish(self.nav_status)
+            # self.feedback_publisher.publish(self.nav_status)
         
         self.published = False
+        self.nav_status.data = 99
         
         # Shutdown the node after receiving the result
         self.node.get_logger().info('Done...')
